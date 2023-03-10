5318b60cfdf152108cddd42be05f46b58a7695c9
htorres4787 <stuwrkr-htorre23@mx.uillinois.edu>
2023-03-10 17:37:55 +0000

    photogram gui

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index"})
 
   get("/users/:path_username", { :controller => "users", :action => "info"})
+
+  get("/photos", { :controller => "photos", :action => "index" })
 end
